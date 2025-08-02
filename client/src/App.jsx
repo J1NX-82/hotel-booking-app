850fa4fa3beff3dd752593d4db5c71431e0887ef
J1NX-82 <rahulgoala082.gmail.com>
2025-08-02 22:40:46 +0530

    Updated Navbar component and fixed routing issues

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from 'react';
+import Navbar from './components/navbar';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
+  const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <div>
-      
+      {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
